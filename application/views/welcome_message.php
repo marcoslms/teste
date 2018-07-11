65c629623055f8fa9500b96dcefd59e3417fce6b
marcoslms <marcoslms25@gmail.com>
2018-07-11 00:42:59 -0300

    texto tela de login
    modificação no tesxto de entrada na tela de login

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,10 @@
                     Bem vindo a aplicação que gerencie tarefas. </p>
                 <p>A aplicação deve cadastrar uma nova tarefa, alterar a tarefa, listar todas as tarefas e deletar a tarefa.</p>
                 <p>Cada usuário só deve visualizar a sua tarefa.    </p>
-                <p>Primeiro acesso deve ser realizado com Login:admin e Senha:admin</p>
+                <p>Segue abaixo 3 usuarios cadastrados para testes:</p>
+                <p>Login:teste1 e Senha:teste1 </p>
+                <p>Login:teste2 e Senha:teste2 </p>
+                <p>Login:teste3 e Senha:teste3 </p>
                 <!-- <p>
                     <a class="btn btn-primary btn-large" href="#">Learn more</a>
                 </p> -->
